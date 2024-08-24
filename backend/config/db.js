dfdc034c1b00dcccd68f052213b3737b371a96a2
Codedrin <aldrinrosales428@gmail.com>
2024-08-24 12:42:06 +0800

    added MongoDB database online

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
     })
     .then(() => {
       console.log("Successfully connected to MongoDB Atlas!");

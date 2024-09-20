5f674fe1ed1aeb669df5beb63df411f0821772e9
s4nm4rkus <sanmarkusmorcoso@gmail.com>
2024-09-20 19:30:31 +0800

    updated link

--- backend/controllers/quotationController.js
+++ backend/controllers/quotationController.js
@@ -35,7 +35,7 @@ export const createQuotation = async (req, res) => {
         console.log('Quotation token saved successfully:', quotationToken);
 
         // Create verification link
-        const verificationLink = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/users/quotation/confirm/${quotationToken.quotationToken}`;
+        const verificationLink = `${process.env.VITE_REACT_APP_BACKEND_BASEURL}/api/users/quotation/confirm/${quotationToken.quotationToken}`;
 
         // Send the verification email
         try {

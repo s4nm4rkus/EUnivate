44adc229ff466854411ba72dd414acb36357d879
Nicko lander <nickolandermilan30@gmail.com>
2024-09-22 22:42:43 +0800

    naoy

---
+++ frontend/src/view/pages/SuperAdmin/Loading Style/Circle Loading/ButtonCircle.jsx
@@ -0,0 +1,17 @@
+// ButtonCircle.js
+import React from 'react';
+import './ButtonCircle.css'; // Ensure this file exists
+
+const ButtonCircle = () => {
+    return (
+        <div className="loading-container">
+            <div className="balls">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default ButtonCircle;
